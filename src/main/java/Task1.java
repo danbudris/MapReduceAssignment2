c8f0b2318f07a847a39d40c1bafe61750d7ebbca
danbudris <d.c.budris@gmail.com>
2018-02-17 15:33:05 -0500

    Initial commit

---
+++ src/main/java/Task1.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Task1 {
+
+    public static class GetErrors extends Mapper<Object, Text, Text, IntWritable>{
+        private final static IntWritable one = new IntWritable(1);
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String line = key.toString();
+            String[] fields = line.split(",");
+            if  (fields.length == 17)
+                if (Double.parseDouble(fields[7]) == 0.000000)
+                    context.write(new Text(fields[3]), one);
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job =  new Job(conf, "task1");
+        job.setJarByClass(Task1.class);
+        job.setMapperClass(Task1.GetErrors.class);
+        job.setCombinerClass(Task1.IntSumReducer.class);
+        job.setReducerClass(Task1.IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
