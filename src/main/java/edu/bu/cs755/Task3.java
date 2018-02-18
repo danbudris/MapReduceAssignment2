43fdb5d7b42bdb849b69c1be0779554bbbbff02f
danbudris <d.c.budris@gmail.com>
2018-02-18 15:17:45 -0500

    troublshooting some infinity errors

--- src/main/java/edu/bu/cs755/Task3.java
+++ src/main/java/edu/bu/cs755/Task3.java
@@ -23,8 +23,8 @@ public class Task3 {
             String line = value.toString();
             // split the string, on commas, into a list of strings
             String[] fields = line.split(",");
-            // only process records with exactly 17 fields, thus discarding some malformed records
-            if  (fields.length == 17 && Double.parseDouble(fields[16]) > 0) {
+            // only process records with exactly 17 fields, the seconds and the fare are greater than 0, thus discarding some malformed records
+            if  (fields.length == 17 && Double.parseDouble(fields[16]) > 0 && Double.parseDouble(fields[4]) > 0) {
                 // get the total fare for the current ride
                 Double fare = Double.parseDouble(fields[16]);
                 // get the number of minutes of the current ride
@@ -32,6 +32,12 @@ public class Task3 {
                 // create the var for output, and set to the dollars per minute for this ride
                 DoubleWritable moneyPerMinute = new DoubleWritable();
                 moneyPerMinute.set(fare/minutes);
+
+                //troubleshooting
+                System.out.println("FARE: " + fare);
+                System.out.println("Minutes: " + minutes);
+                System.out.println("moneyPerMinute: " + moneyPerMinute);
+
                 // write to the context the medallion number and money per minute for this ride
                 context.write(new Text(fields[0]), moneyPerMinute);
             }
@@ -53,8 +59,9 @@ public class Task3 {
                 // add to the total money per minute
                 moneyPerMinute += val.get();
             }
-            System.out.println(totalRides);
-            System.out.println(moneyPerMinute);
+            // troubleshooting
+            System.out.println("Total Rides:" + totalRides);
+            System.out.println("Money Per Minute: " + moneyPerMinute);
             // set the result to the money per minute total divided by the trips total, which is the average money per minute
             result.set(moneyPerMinute/totalRides);
             //write to the context the medallion number and money per minute for this ride
