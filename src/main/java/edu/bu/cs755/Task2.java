8e97ec7184a64cce47e269a9d801ee92649b9907
danbudris <d.c.budris@gmail.com>
2018-02-18 10:31:52 -0500

    Created logic for generating error rates per medallion number

--- src/main/java/edu/bu/cs755/Task2.java
+++ src/main/java/edu/bu/cs755/Task2.java
@@ -24,6 +24,7 @@ public class Task2 {
 
         // Set the variable which will be the value in the output map
         private final static IntWritable one = new IntWritable(1);
+        private final static IntWritable two = new IntWritable(2);
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
@@ -34,9 +35,14 @@ public class Task2 {
                 System.out.println(str + "\n");
             }
             System.out.println(fields.length);
+            // only process records with exactly 17 fields, thus discarding malformed records
             if  (fields.length == 17)
+                // if the record contains GPS errors, set the value to 1
                 if (Double.parseDouble(fields[6]) == 0.000000 && Double.parseDouble(fields[7]) == 0.000000 && Double.parseDouble(fields[8]) == 0.000000 && Double.parseDouble(fields[9]) == 0.000000 )
                     context.write(new Text(fields[0]), one);
+                // if it does not have erros, set the value to 2
+                else
+                    context.write(new Text(fields[0]), two);
         }
     }
 
@@ -45,11 +51,19 @@ public class Task2 {
         public void reduce(Text key, Iterable<IntWritable> values,
                            Context context
         ) throws IOException, InterruptedException {
-            int sum = 0;
+            // Store the number of error records
+            int errSum = 0;
+            // Store the total number of records
+            int totalSum = 0;
             for (IntWritable val : values) {
-                sum += val.get();
+                // increment the total number of records
+                totalSum += 1;
+                if (val.get() == 1)
+                    // if the value is 1 (an error record) increment the error sum
+                    errSum += 1;
             }
-            result.set(sum);
+            // set the result to the percentage of error records in the total records for the given medallion number
+            result.set(errSum/totalSum);
             context.write(key, result);
         }
     }
